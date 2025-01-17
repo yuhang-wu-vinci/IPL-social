919cada3049e16aed8e3c0b1799affecef60536e
yuhang-wu-vinci <yuhang.wu@student.vinci.be>
2025-01-17 17:29:12 +0100

    add hasSpecialCharacter

--- passwordChecker.js
+++ passwordChecker.js
@@ -3,7 +3,10 @@ const hasMinLength = (password) => {
     return password.length >= MIN_LENGTH;
 };
 
-
+const hasSpecialCharacter = (password) => {
+    const SPECIAL_CHAR_PATTERN = /[!@#$%^&*(),.?":{}|<>]/;
+    return SPECIAL_CHAR_PATTERN.test(password);
+};
 
 module.exports = {
     hasMinLength,
--- spec/passwordChecker.spec.js
+++ spec/passwordChecker.spec.js
@@ -1,4 +1,5 @@
 const { hasMinLength } = require('../passwordChecker');
+const { hasSpecialCharacter } = require('../passwordChecker');
 
 
 //test de la taille du mdp
@@ -19,3 +20,31 @@ describe('hasMinLength', () => {
     expect(hasMinLength('')).toBe(false);
   });
 });
+
+
+//test a un character spécial
+describe('hasSpecialCharacter', () => {
+    it('devrait retourner true si le mot de passe contient un caractère spécial', () => {
+      expect(hasSpecialCharacter('Valid123!')).toBe(true);
+    });
+  
+    it('devrait retourner false si le mot de passe ne contient pas de caractère spécial', () => {
+      expect(hasSpecialCharacter('Valid123')).toBe(false);
+    });
+  
+    it('devrait retourner true si le mot de passe contient plusieurs caractères spéciaux', () => {
+      expect(hasSpecialCharacter('Valid!@#123')).toBe(true);
+    });
+  
+    it('devrait retourner false pour un mot de passe vide', () => {
+      expect(hasSpecialCharacter('')).toBe(false);
+    });
+  
+    it('devrait retourner true si le mot de passe commence par un caractère spécial', () => {
+      expect(hasSpecialCharacter('!Valid123')).toBe(true);
+    });
+  
+    it('devrait retourner true si le mot de passe se termine par un caractère spécial', () => {
+      expect(hasSpecialCharacter('Valid123!')).toBe(true);
+    });
+  });
